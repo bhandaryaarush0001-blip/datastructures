4dca6ae94cb572a2b1562fb8d7ee1db4474ac8a6
bhandaryaarush0001-blip <bhandaryaarush0001@gmail.com>
2026-03-21 22:02:51 +0530

    file handling paart-1

---
+++ main.py
@@ -0,0 +1,25 @@
+x = ()
+print(x)
+
+y = (1,2,3)
+print(y)
+
+z = (1,"hello",3.14)
+print(z)
+
+w = ("mouse",[8,4,6],[1,2,3])
+print(w)
+
+t = ('p','e','r','m','i','t')
+print(t[0])
+print(t[5])
+
+a = ("mouse",[8,4,6],(1,2,3))
+print(a[0][3])
+print(a[1,1])
+
+print("Sliced:",a[1:4])
+
+for i in (a):
+    print("Hello",i)
+
